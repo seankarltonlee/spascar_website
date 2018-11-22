39d5714a3cc0cfa109d6447828896875552e61d5
seankarltonlee <seankl@usc.edu>
2018-11-21 22:45:42 -0800

    Updates

---
+++ precache-manifest.3a11bccd7cacb832108f361e92982bea.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3c060958e1dacd0458643a84f34f595",
+    "url": "/spascar_website/static/media/tiefighter.e3c06095.jpg"
+  },
+  {
+    "revision": "61355686131057e461bbaed00a7f8d1b",
+    "url": "/spascar_website/static/media/sickmenu.61355686.png"
+  },
+  {
+    "revision": "edea0532183f8d26baf15589092ed956",
+    "url": "/spascar_website/static/media/homebase.edea0532.png"
+  },
+  {
+    "revision": "83694fc46f6206b862ee85e7531afa9b",
+    "url": "/spascar_website/static/media/cockpit.83694fc4.png"
+  },
+  {
+    "revision": "363cf3b825eba722173b42be580fba11",
+    "url": "/spascar_website/static/media/asteroids.363cf3b8.png"
+  },
+  {
+    "revision": "4944561f114a09f2b54b",
+    "url": "/spascar_website/static/js/runtime~main.4944561f.js"
+  },
+  {
+    "revision": "32771191e0d96dc57d66",
+    "url": "/spascar_website/static/js/main.32771191.chunk.js"
+  },
+  {
+    "revision": "795935fa0a4682b290af",
+    "url": "/spascar_website/static/js/1.795935fa.chunk.js"
+  },
+  {
+    "revision": "32771191e0d96dc57d66",
+    "url": "/spascar_website/static/css/main.2a7f0eae.chunk.css"
+  },
+  {
+    "revision": "1835e51a6dc89d7ae7251f318cc86ae4",
+    "url": "/spascar_website/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/spascar_website/precache-manifest.3a11bccd7cacb832108f361e92982bea.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/spascar_website/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
